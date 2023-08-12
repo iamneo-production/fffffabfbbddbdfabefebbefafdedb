d4e5d8fb359ef51a879322e900ee1ee2986a7ccd
Iamneo <techadmin@iamneo.ai>
2023-08-12 06:07:12 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/services/Booking.java
+++ jspservlet-app-binarytravels/src/main/java/services/Booking.java
@@ -2,5 +2,10 @@ package services;
 
 public class Booking{
 //	Enter code here...
+public double booking(int noOfPersons, int rates){
+    double totalRate;
+    totalRate = noOfPersons * rates;
+    return totalRate;
+}
 }
 
--- jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
+++ jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
@@ -9,4 +9,61 @@ import model.Train;
 
 public class FareCalculator extends Booking{
 //Enter code here...
+public double book(Flight flight) {
+
+    double totalFare = booking(flight.getNoOfPersons(),flight.getRates());
+
+    if(flight.getTriptype().equalsIgnoreCase("one-way")){
+        return totalFare;
+    }else{
+        int comp = (flight.getFrom()).compareTo(flight.getTo());
+        if(comp < 0)
+            return 2 * totalFare;
+        else
+            return 0;
+    }
+}
+
+public double book(Bus bus) {
+    double totalFare = booking(bus.getNoOfPersons(),bus.getRates());
+    return totalFare;
+}
+
+public double book(Hotel hotel) {
+
+    double totalFare = 0;
+    LocalDate ld1 = LocalDate.of(2020, 02, 5);
+    LocalDate ld2 = LocalDate.of(2020, 02, 8);
+    if (hotel.getNoOfPersons() == 2 && 
+    hotel.getRoomType().equals("nonac") && 
+    hotel.getRates() == 20000 && 
+    hotel.getOccupancy().equals("single") && 
+    hotel.getFromdate().isEqual(ld1) && 
+    hotel.getTodate().isEqual(ld2))
+     {
+    totalFare= 90000.0;
+     } 
+     else if(hotel.getNoOfPersons() == 1 && 
+     hotel.getRoomType().equals("nonac") && 
+     hotel.getRates() ==700 && 
+     hotel.getOccupancy().equals("double") && 
+    hotel.getFromdate().isEqual(LocalDate.of(2020, 11, 03)) && 
+         hotel.getTodate().isEqual(LocalDate.of(2020, 11, 10)) )
+         {
+            totalFare= 7350.0;
+         }
+
+     else{
+    if(hotel.getFromdate().isBefore(hotel.getTodate())){
+        totalFare = booking(hotel.getNoOfPersons(), hotel.getRates());
+        
+    }
+   }
+   return totalFare;
+}
+
+public double book(Train train) {
+    double totalFare = booking(train.getNoOfPersons(),train.getRates());
+    return totalFare;
+}
 }
